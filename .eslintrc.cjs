51c339ed4aca47a40ead65dda4b95feecdbad8e0
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-09 22:07:53 -0700

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
+  plugins: ['prettier', 'node'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+  },
+  rules: {
+    // See https://github.com/mysticatea/eslint-plugin-node/issues/255 for more info.
+    'node/no-missing-import': [
+      'error',
+      {
+        allowModules: ['release-git'],
+      },
+    ],
+    'prettier/prettier': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      files: ['jest.setup.js', '__tests__/**/*.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
